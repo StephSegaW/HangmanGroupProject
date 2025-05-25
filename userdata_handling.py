a4b35f8bc9a90966af485a373e23bb71732040a6
Magda Rembowska <magda.rembowska@gmail.com>
2025-05-25 15:59:49 +0200

    I tried to modify the userdata file, but I got stuck halfway through.

--- userdata_handling.py
+++ userdata_handling.py
@@ -1,6 +1,5 @@
 #Magda
 
-import time
 #class UserData:                                     # The main class that holds all the data related to a single player session.
 #    def __init__(self, username):
 #       self.username = username                    # the name of the player
@@ -11,3 +10,25 @@ import time
 
 #Discuss if we need to switch to a dictionary
 #add function to save the data
+
+
+import random
+from datetime import datetime
+
+def select_random_word(word_list):
+    return random.choice(word_list)
+
+def update_guessed_word(secret_word, current_state, guessed_letter):
+    return "", join([
+        guessed_letter if secret_word[i] == guessed_letter else current_state[i]
+        for i in range(len(secret_word))
+    ])
+
+def check_game_result(secret_word, current_state, attempts_left):
+    if current_state == secret_word:
+        return "win"
+    elif attempts_left <= 0:
+        return "lost"
+    else:
+        return "ongoing"
+    
